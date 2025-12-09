04a84dbe56a940aba0a5f3ee2eca36536c673cd2
joomkit <alan@craftcode.co.uk>
2025-12-09 17:51:34 +0000

    profile

--- config/app.php
+++ config/app.php
@@ -23,6 +23,7 @@ use craft\helpers\App;
 use modules\fetchprofile\FetchProfile;
 use modules\notifysave\NotifySave;
 
+
 return [
     'id' => App::env('CRAFT_APP_ID') ?: 'CraftCMS', 
     'modules' => [
